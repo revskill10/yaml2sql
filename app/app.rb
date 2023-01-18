9ea58eb80a1e8002129c7b13bc75455f6e0a5eea
Truong Dung <truonghoangdung1987hp@gmail.com>
2023-01-18 14:15:25 +0700

    V1

--- app/app.rb
+++ app/app.rb
@@ -18,19 +18,19 @@ class App < Sinatra::Base
 
   set :mustache, {
     views: "app/views",
-    templates: "templates",
+    templates: "app/templates",
     namespace: App,
   }
 
   #set :show_exceptions, false
   set :runner, PgQueryRunner.new
-  #   error NotImplementedError do
-  #     content_type :json
-  #     status 400 # or whatever
+  error NotImplementedError do
+    content_type :json
+    status 400 # or whatever
 
-  #     e = env["sinatra.error"]
-  #     { :result => "error", :message => e.message }.to_json
-  #   end
+    e = env["sinatra.error"]
+    { :result => "error", :message => e.message }.to_json
+  end
   get "/docs" do
     view_name = params[:view] || "hello"
     f = File.read(__dir__ + "/../templates/docs/#{view_name.to_sym}.md")
@@ -38,28 +38,8 @@ class App < Sinatra::Base
     mustache :docs
   end
 
-  get "/play/:view?" do
+  get ["/", "/play/:view?"] do
     view_name = params[:view] || "simple"
-    puts "View #{view_name}"
-    @y = File.read(__dir__ + "/queries/#{view_name.to_sym}.yml")
-
-    c = @y.gsub(/on:/, "where:")
-
-    r = Psych.safe_load(c, aliases: true, symbolize_names: true, permitted_classes: [Date])
-    puts "SSS: #{r}"
-    v = YamlView.new
-    v.process(r)
-    begin
-      @h = settings.runner.run!(v.ctx)
-      mustache :simple
-    rescue => e
-      e.message
-      e.backtrace
-    end
-  end
-  get "/" do
-    view_name = params[:view] || "simple"
-    puts "View #{view_name}"
     @y = File.read(__dir__ + "/queries/#{view_name.to_sym}.yml")
 
     c = @y.gsub(/on:/, "where:")
